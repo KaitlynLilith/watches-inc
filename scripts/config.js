c7c2f32b214b86b09f5d602a9dc690c3f13eaf58
KaitlynLilith <Oldsmobile86_307@yahoo.com>
2022-07-12 15:26:16 -0500

    Added more files

---
+++ scripts/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://s-watches-api.herokuapp.com/api/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY =
+  "07d8b6e9-b4a5-453a-bd27-def805ae4bb6cf2e10c5-1b7f-47a4-9ca6-1b76deaed3f4";
